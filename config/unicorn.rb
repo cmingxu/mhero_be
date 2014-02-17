8b70c6af314cf6a4a5ce445d625f87453d2f179e
Kevin.Xu <cmxu@kabam.com>
2014-02-17 22:05:18 +0800

    deploy & unicorn

---
+++ config/unicorn.rb
@@ -0,0 +1,17 @@
+root = Rails.env.development? ? File.expand_path(File.join(File.dirname(__FILE__), "..")) : "/home/www/code/current"
+
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.mhero_be.sock"
+
+worker_processes 2
+timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
