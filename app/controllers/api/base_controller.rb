970d818dae541739ebc1c2c510c0d1ff81cc65cd
Kevin.Xu <cmxu@kabam.com>
2014-02-24 15:26:44 +0800

    change mongo session dynamiclly

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,6 @@
 class Api::BaseController < ApplicationController
+  before_filter :switch_to_right_database
+
   skip_before_filter :verify_authenticity_token
 
   def api_success(payload)
@@ -9,5 +11,9 @@ class Api::BaseController < ApplicationController
   def api_error(payload)
     default = {:success => false}
     default.merge(payload)
+  end
+
+  def switch_to_right_database
+    WorldBase.switch_to_db "mhero_world3_dev" 
   end
 end
--- app/models/world_base.rb
+++ app/models/world_base.rb
@@ -1,4 +1,7 @@
 class WorldBase
-  def self.switch_to_db(spec)
+  def self.switch_to_db(db_name)
+    self.subclasses.each do |sub|
+      sub.store_in(:database => db_name)
+    end
   end
 end
